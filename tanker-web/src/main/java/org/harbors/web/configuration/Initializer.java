caf24c0b3d63735d8dc0efc1a05994c972931126
Nagi Letaifa <nagi.letaifa@gmail.com>
2013-10-28 22:37:34 +0100

    First controller

---
+++ tanker-web/src/main/java/org/harbors/web/configuration/Initializer.java
@@ -0,0 +1,32 @@
+package org.harbors.web.configuration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class Initializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext mvcContext = getAnnotationConfigWebApplicationContext(servletContext);
+        configureDispatcher(servletContext, mvcContext);
+    }
+
+    private AnnotationConfigWebApplicationContext getAnnotationConfigWebApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
+        mvcContext.setServletContext(servletContext);
+        mvcContext.register(MvcConfig.class);
+        return mvcContext;
+    }
+
+    private void configureDispatcher(ServletContext servletContext, AnnotationConfigWebApplicationContext mvcContext) {
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("dispatcher", new DispatcherServlet(mvcContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+}
---
+++ tanker-web/src/main/java/org/harbors/web/configuration/MvcConfig.java
@@ -0,0 +1,56 @@
+package org.harbors.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"org.harbors"})
+//@ImportResource({"classpath:database-config.xml", "classpath:security-config.xml"})
+@EnableTransactionManagement
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ServletContextTemplateResolver templateResolver = getServletContextTemplateResolver();
+        SpringTemplateEngine engine = getSpringTemplateEngine(templateResolver);
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine);
+        return viewResolver;
+    }
+
+    private ServletContextTemplateResolver getServletContextTemplateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setPrefix("/html/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheable(false); // set to true in production
+        return templateResolver;
+    }
+
+    private SpringTemplateEngine getSpringTemplateEngine(ServletContextTemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+        return engine;
+    }
+
+    /**
+     * Manage static ressources like mvc:resources in xml
+     * http://stackoverflow.com/questions/14861720/annotation-configuration-replacement-for-mvcresources-spring
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+}
